7046c027740b04006c3521a438333b96d88b8f8c
MuriloRieth <167137795+MuriloRieth@users.noreply.github.com>
2024-11-18 19:36:10 -0300

    Create TrainingsFragment.kt

---
+++ TrainingsFragment.kt
@@ -0,0 +1,15 @@
+package com.exemplo.gestaodesporte
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class TrainingsFragment : Fragment(R.layout.fragment_trainings) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Lógica para exibir os treinos
+    }
+}
